1229d48014c613846504232d4ce3e5ced8ce8c79
Austin Beaufort <tinyhatcoding@gmail.com>
2019-06-22 10:16:00 -0400

    notes on variables, arithmetic, string concatenation, data types, and constants'

--- variables.php
+++ variables.php
@@ -1,17 +1,74 @@
 
 
 
+<?php
+    #ARITHMETIC
+    // $num1 = 4;
+    // $num2 = 10;
+    // $sum = $num1 + $num2;
+    // echo $sum;
+
+    #CONCATENATING STRINGS
+    // $string1 = 'Hello';
+    // $string2 = 'World';
+    // $greeting = $string1 .' '. $string2.'!';
+    // echo $greeting;
+
+    // $greeting2 = "$string1 $string2";
+    // echo $greeting2;
+
+    #ESCAPE SEQUENCE
+    // $string3 = 'They\'re Here';
+    // echo $string3;
+
+    #CONSTANTS
+    // define('GREETING', 'Hello Everyone');
+    // echo GREETING;
+?>
 
 
-// Single line comment
+<!-- <?php
+    #DATA TYPES 
+    // String
+
+    // Integers
+    $num1 = 4;
+    echo $num1;
+
+
+    // floats
+    $float1 = 4.4;
+    echo $float1;
+
+    // Booleans
+    $bool1 = true;
+    echo $bool1;
+
+    // Arrays
+    // Objects
+    // NULL
+    // Resource
+
+
+    #VARIABLES
+    // Prefix with $
+    // Start with a letter or an underscore
+    // Only letters, numbers, and underscores
+    // Case sensitive
+
+    $output = 'Hello world!';
+    echo $output;
+?> -->
+
+
+<!-- 
+<?php
+    echo 'Hello World';
+    // Single line comment
     # Single line comment
     /*
     multi
     line
     comment
     */
-
-<!-- 
-<?php
-    echo 'Hello World';
 ?> -->
